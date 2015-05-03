16e1310969a5c0f91bf47ee0cfdba26e010e0b68
Mike Barlow <snsmurf@gmail.com>
2015-05-04 00:18:08 +0100

    extracted out the list building code and added OL shortcut. Also added tests for list related methods

--- Tests/Helpers/HelpersHtmlTest.php
+++ Tests/Helpers/HelpersHtmlTest.php
@@ -82,4 +82,69 @@ class HelpersHtmlTest extends \PHPUnit_Framework_TestCase
             )
         );
     }
+
+    public function testOlReturnsValidOlElement()
+    {
+        $Html = $this->getHtml();
+
+        $this->assertSame(
+            '<ol id="main ol"><li>this is a list item</li><li>this is a list item 2</li><li>sub-item<ol><li>will they work?</li><li>or won\'t they, who knows?</li></ol></li><li id="listID">subitem attr!<ol id="sub-list"><li>this is a sub item</li><li>this list should have attributes</li></ol></li></ol>',
+            $Html->ol(
+                array(
+                    'this is a list item',
+                    'this is a list item 2',
+                    'sub-item' => array(
+                        'will they work?',
+                        'or won\'t they, who knows?'
+                    ),
+                    'subitem attr!' => array(
+                        'attr' => array(
+                            'id' => 'listID'
+                        ),
+                        'ulAttr' => array(
+                            'id' => 'sub-list'
+                        ),
+                        'list' => array(
+                            'this is a sub item',
+                            'this list should have attributes'
+                        )
+                    )
+                ),
+                array(
+                    'id' => 'main ol'
+                )
+            )
+        );
+    }
+
+    public function testProcessListReturnsValidElements()
+    {
+        $Html = $this->getHtml();
+
+        $this->assertSame(
+            '<li>this is a list item</li><li>this is a list item 2</li><li>sub-item<ul><li>will they work?</li><li>or won\'t they, who knows?</li></ul></li><li id="listID">subitem attr!<ul id="sub-list"><li>this is a sub item</li><li>this list should have attributes</li></ul></li>',
+            $Html->processList(
+                array(
+                    'this is a list item',
+                    'this is a list item 2',
+                    'sub-item' => array(
+                        'will they work?',
+                        'or won\'t they, who knows?'
+                    ),
+                    'subitem attr!' => array(
+                        'attr' => array(
+                            'id' => 'listID'
+                        ),
+                        'ulAttr' => array(
+                            'id' => 'sub-list'
+                        ),
+                        'list' => array(
+                            'this is a sub item',
+                            'this list should have attributes'
+                        )
+                    )
+                )
+            )
+        );
+    }
 }
--- src/Helpers/Html.php
+++ src/Helpers/Html.php
@@ -40,22 +40,59 @@ trait Html
         $out = $this->tag('ul', $attr);
 
         if (! empty($list) && is_array($list)) {
-            foreach ($list as $key => $value) {
-                if (is_array($value) && (isset($value['list']) || isset($value['attr']))) {
-                    $attr = (isset($value['attr'])) ? $value['attr'] : null;
-                    $ulAttr = (isset($value['ulAttr'])) ? $value['ulAttr'] : null;
-                    $subList = (isset($value['list'])) ? $this->ul($value['list'], $ulAttr) : '';
-
-                    $out .= $this->tag('li', $attr, $key . $subList, true);
-                } elseif (is_array($value)) {
-                    $out .= $this->tag('li', null, $key . $this->ul($value), true);
-                } else {
-                    $out .= $this->tag('li', null, $value, true);
-                }
-            }
+            $out .= $this->processList($list);
         }
 
         $out .= $this->tag('/ul');
+
+        return $out;
+    }
+
+    /**
+     * ordered list
+     *
+     * @param   array   Array of items to list / attr
+     * @param   array   Array of attributes for the ul
+     * @return  string
+     */
+    public function ol($list, $attr = null)
+    {
+        $out = $this->tag('ol', $attr);
+
+        if (! empty($list) && is_array($list)) {
+            $out .= $this->processList($list, 'ol');
+        }
+
+        $out .= $this->tag('/ol');
+
+        return $out;
+    }
+
+    /**
+     * process list items
+     *
+     * @todo    add ability to define sublists from within the list
+     * @param   array   Array of list items to process
+     * @param   string  method to use for sub-lists
+     * @return  string
+     */
+    public function processList($list, $subListType = 'ul')
+    {
+        $out = '';
+
+        foreach ($list as $key => $value) {
+            if (is_array($value) && (isset($value['list']) || isset($value['attr']))) {
+                $attr = (isset($value['attr'])) ? $value['attr'] : null;
+                $ulAttr = (isset($value['ulAttr'])) ? $value['ulAttr'] : null;
+                $subList = (isset($value['list'])) ? $this->{$subListType}($value['list'], $ulAttr) : '';
+
+                $out .= $this->tag('li', $attr, $key . $subList, true);
+            } elseif (is_array($value)) {
+                $out .= $this->tag('li', null, $key . $this->{$subListType}($value), true);
+            } else {
+                $out .= $this->tag('li', null, $value, true);
+            }
+        }
 
         return $out;
     }
