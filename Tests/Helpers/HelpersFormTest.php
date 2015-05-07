8184a85bb4bd3978eab47bbfca3958b687773d66
Mike Barlow <snsmurf@gmail.com>
2015-05-07 21:17:01 +0100

    Created the label method, input, button, select, textarea methods and tests

--- Tests/Helpers/HelpersFormTest.php
+++ Tests/Helpers/HelpersFormTest.php
@@ -44,5 +44,100 @@ class HelpersFormTest extends \PHPUnit_Framework_TestCase
         );
     }
 
+    public function testLabelReturnsValidElement()
+    {
+        $Html = $this->getHtml();
 
+        $this->assertSame(
+            '<label for="dataYourEmail">Your Email</label>',
+            $Html->Form->label(
+                'Your Email',
+                array(
+                    'for' => 'dataYourEmail'
+                )
+            )
+        );
+    }
+
+    public function testGenerateFieldReturnsValidInputElement()
+    {
+        $Html = $this->getHtml();
+
+        $this->assertSame(
+            '<input type="text" class="required" name="your_name">',
+            $Html->Form->generateField(
+                'your_name',
+                array(
+                    'type' => 'text',
+                    'class' => 'required'
+                )
+            )
+        );
+    }
+
+    public function testGenerateOptionsReturnsValidSelectOptions()
+    {
+        $Html = $this->getHtml();
+
+        $this->assertSame(
+            '<option value="test1">Test One</option><option value="test2">Test Two</option>',
+            $Html->Form->generateOptions(
+                array(
+                    'test1' => 'Test One',
+                    'test2' => 'Test Two'
+                )
+            )
+        );
+    }
+
+    public function testGenerateTextareaFieldReturnsValidTextarea()
+    {
+        $Html = $this->getHtml();
+
+        $this->assertSame(
+            '<textarea cols="10" rows="5" name="message"></textarea>',
+            $Html->Form->generateTextareaField(
+                array(
+                    'cols' => '10',
+                    'rows' => '5',
+                    'name' => 'message'
+                )
+            )
+        );
+    }
+
+    public function testGenerateButtonFieldReturnsValidButton()
+    {
+        $Html = $this->getHtml();
+
+        $this->assertSame(
+            '<button class="btn" name="submit">Submit Message</button>',
+            $Html->Form->generateButtonField(
+                array(
+                    'class' => 'btn',
+                    'name' => 'submit',
+                    'value' => 'Submit Message'
+                )
+            )
+        );
+    }
+
+    public function testGenerateSelectFieldReturnsValidSelect()
+    {
+        $Html = $this->getHtml();
+
+        $this->assertSame(
+            '<select class="title" name="title"><option value="mr">Mr</option><option value="mrs">Mrs</option></select>',
+            $Html->Form->generateSelectField(
+                array(
+                    'class' => 'title',
+                    'name' => 'title',
+                    'options' => array(
+                        'mr' => 'Mr',
+                        'mrs' => 'Mrs'
+                    )
+                )
+            )
+        );
+    }
 }
--- src/Helpers/Form.php
+++ src/Helpers/Form.php
@@ -7,6 +7,16 @@ class Form
     protected $Html;
 
     /**
+     * input types requiring custom tag
+     * @param   array
+     */
+    public $nonInputTypes = array(
+        'textarea',
+        'select',
+        'button'
+    );
+
+    /**
      * construct
      * setup the class and store the HTML object
      *
@@ -53,5 +63,118 @@ class Form
     public function close()
     {
         return $this->Html->tag('/form');
+    }
+
+    /**
+     * generate a label
+     *
+     * @param   string  Label Text
+     * @param   array   Array of attributes for the label tag
+     * @return  string
+     */
+    public function label($text, $attr = array())
+    {
+        return $this->Html->tag('label', $attr, $text, true);
+    }
+
+    /**
+     * generate a field
+     *
+     * @param   string  The type of input field to create
+     * @param   array   Array of attributes for the tag
+     * @return  string
+     */
+    public function generateField($name, $attr = array())
+    {
+        $tag = 'input';
+
+        if (empty($attr['type'])) {
+            $attr['type'] = 'text';
+        }
+
+        if (in_array($attr['type'], $this->nonInputTypes)) {
+            $tag = $attr['type'];
+        }
+
+        $attr['name'] = $name;
+
+        if ($tag !== 'input') {
+            $generate = 'generate' . ucfirst(strtolower($tag)) . 'Field';
+            return $this->{$generate}($attr);
+        } else {
+            return $this->Html->tag('input', $attr);
+        }
+    }
+
+    /**
+     * generate a textarea
+     *
+     * @param   array   array of attributes
+     * @return  string
+     */
+    public function generateTextareaField($attr)
+    {
+        return $this->Html->tag('textarea', $attr, '', true);
+    }
+
+    /**
+     * generate a button
+     *
+     * @param   array   array of attributes
+     * @return  string
+     */
+    public function generateButtonField($attr)
+    {
+        $value = 'Submit';
+
+        if (! empty($attr['value'])) {
+            $value = $attr['value'];
+        }
+        unset($attr['value']);
+
+        return $this->Html->tag('button', $attr, $value, true);
+    }
+
+    /**
+     * generate a select field
+     *
+     * @param   array   array of attributes
+     * @return  string
+     */
+    public function generateSelectField($attr)
+    {
+        $options = array();
+
+        if (! empty($attr['options'])) {
+            $options = $attr['options'];
+        }
+        unset($attr['options']);
+        $options = $this->generateOptions($options);
+
+        return $this->Html->tag('select', $attr, $options, true);
+    }
+
+    /**
+     * generate the options for a select box
+     *
+     * @param   array   array of options
+     * @return  string
+     */
+    public function generateOptions($options)
+    {
+        $return = '';
+
+        foreach ($options as $key => $value) {
+            $return .= $this->Html->tag(
+                'option',
+                array(
+                    'value' => $key
+                ),
+                $value,
+                true
+            );
+        }
+
+        return $return;
     }
 }
